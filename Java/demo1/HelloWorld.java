c5150ba94195ed278741d61c7bac7814cc8d4ac7
wangyi <1664233781@qq.com>
2022-03-09 19:36:33 +0800

    3.09

---
+++ Java/demo1/HelloWorld.java
@@ -0,0 +1,7 @@
+package Java.demo1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
---
+++ Java/src/Test.java
@@ -0,0 +1,12 @@
+import java.util.SortedSet;
+
+/**
+ * @author wang
+ * @create 2022-01-26 23:54
+ */
+public class Test {
+    public static void main(String[] args) {
+        int T;
+        System.out.println();
+    }
+}
